a22d0015111aa196ebcf26107b688481efad8339
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-04-07 11:03:36 +0900

    feat: add card shadow

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import "../styles/Card.css";
 
 export default function Card({ card, onClick }) {
+  const [isComplete, setIsComplete] = useState(false);
+
   return (
     <div className="card" id={card.id} onClick={onClick}>
       <div className="image-wrapper">
-        <img src={card.imgUrl} alt={`Image of ${card.name}`} />
+        <img
+          src={card.imgUrl}
+          alt={`Image of ${card.name}`}
+          onLoad={() => setIsComplete(true)}
+        />
       </div>
-      <span className="pokemon-name">{card.name}</span>
+      {!isComplete && <div className="loading">Loading...</div>}
+      <span className="pokemon-name">
+        {card.name.at(0).toUpperCase() + card.name.slice(1)}
+      </span>
     </div>
   );
 }
